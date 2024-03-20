d8b520a4afd559dcbd42283b3373493d7d67a8b4
Ethan Cavill <ethancavill@gmail.com>
2024-03-20 13:54:48 -0700

    html tag change

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -1,3 +1,3 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  return <div className="prose prose-slate">{children}</div>;
+  return <article className="prose prose-slate">{children}</article>;
 }
