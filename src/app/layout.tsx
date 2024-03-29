a42fe3f25ac86d15eb6a997f3433d5c93aa30059
Ethan Cavill <ethancavill@gmail.com>
2024-03-29 11:10:39 -0700

    Add MIT License

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
-'use client';
-
 import Header from '@/app/components/header';
 import Footer from '@/app/components/footer';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
+import { Metadata } from 'next';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Ethan Cavill',
+  description: 'AI Developer and Owner',
+};
 
 export default function RootLayout({
   children,
@@ -15,16 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="../../public/favicons/favicon.ico" />
-        <link rel="icon" href="../../public/favicons/android-chrome-192x192.png" />
-        <link rel="icon" href="../../public/favicons/apple-touch-icon.png" />
-        <link rel="icon" href="../../public/favicons/favicon-32x32.png" />
-        <title>EC</title>
-        <meta name="description" content="Ethan's portfolio and blog" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <body className={inter.className}>
         <Header />
         {children}
