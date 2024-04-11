1887f870ad425838e1c167921f77d88ecd6a7ee1
Ethan Cavill <ethancavill@gmail.com>
2024-04-11 13:28:43 -0700

    Fixes: icons, prettier, lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import Header from '@/app/components/header';
 import Footer from '@/app/components/footer';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,6 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="../../public/favicons/icon.png" sizes="any" />
+      </Head>
+
       <body className={inter.className}>
         <Header />
         {children}
