7ec047fcec41471fbfc858d59e3c2b0948be4ae7
Ethan Cavill <ethancavill@gmail.com>
2024-04-12 14:16:09 -0700

    Fixes to mdx file finder

--- src/utils/mdxUtils.ts
+++ src/utils/mdxUtils.ts
@@ -6,19 +6,27 @@ import path from 'path';
 
 const POSTS_FOLDER = path.join(process.cwd(), 'src/app/blog/posts');
 
+// Helper function to recursively get all MDX files in a directory
+async function getMdxFiles(dir: string): Promise<string[]> {
+  let files = await fs.readdir(dir, { withFileTypes: true });
+  const mdxFiles = files.map(async (file) => {
+    const res = path.resolve(dir, file.name);
+    return file.isDirectory() ? getMdxFiles(res) : path.extname(file.name) === '.mdx' ? res : [];
+  });
+
+  return (await Promise.all(mdxFiles)).flat();
+}
+
 export const getPosts = cache(async (): Promise<Post[]> => {
-  const posts = await fs.readdir(POSTS_FOLDER);
+  const filePaths = await getMdxFiles(POSTS_FOLDER);
 
   return Promise.all(
-    posts
-      .filter((file) => path.extname(file) === '.mdx')
-      .map(async (file) => {
-        const filePath = path.join(POSTS_FOLDER, file);
-        const postContent = await fs.readFile(filePath, 'utf8');
-        const { data, content } = matter(postContent);
+    filePaths.map(async (filePath) => {
+      const postContent = await fs.readFile(filePath, 'utf8');
+      const { data, content } = matter(postContent);
 
-        return { ...data, body: content } as Post;
-      }),
+      return { ...data, body: content } as Post;
+    }),
   ).then((posts) => posts.filter((post): post is Post => post !== null));
 });
 
