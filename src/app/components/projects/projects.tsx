27ba0bcf7fce2d26a8dc90e9e6ca16d0f4870e3a
Ethan Cavill <ethancavill@gmail.com>
2024-04-11 17:51:08 -0700

    Add more projects

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -1,5 +1,9 @@
 import michellesImage from '../../../../public/home/michelles-portfolio.png';
-import ethansImage from '../../../../public/home/my-portfolio.png';
+import vectorsearchImage from '../../../../public/home/vector-search.png';
+import radreportImage from '../../../../public/home/radreport.webp';
+import portfolioImage from '../../../../public/home/portfolio.png';
+import deepworkImage from '../../../../public/home/deep-work.png';
+import devarityImage from '../../../../public/home/devarity.png';
 import { StaticImageData } from 'next/image';
 
 import ProjectSlideClient from './project-slide-client';
@@ -10,7 +14,7 @@ export interface ProjectSlideInterface {
   title: string;
   description: string;
   technologies: string[];
-  viewLink: string;
+  viewLink?: string;
 }
 
 export interface ProjectSlideProps extends ProjectSlideInterface {
@@ -19,21 +23,76 @@ export interface ProjectSlideProps extends ProjectSlideInterface {
 
 const projects: ProjectSlideInterface[] = [
   {
+    imageSrc: deepworkImage,
+    title: 'Deep Work Timer',
+    description:
+      'A project still in development that aims to provide tools for increasing productivity. After reading Deep Work, by Cal Newport, I was disappointed at the array of productivity tools on the market. This inspired an idea to develop my ideal productivity tracker in-line with Newport’s research.',
+    technologies: [
+      'NextJS',
+      'Docker',
+      'AWS',
+      'Redis',
+      'Postgres',
+      'Jotai',
+      'GraphQL',
+      'Strawberry',
+      'FastAPI',
+      'SQLModel',
+    ],
+    viewLink: 'https://github.com/earcherc/deep-work',
+  },
+  {
+    imageSrc: vectorsearchImage,
+    title: 'Tangible AI Internship',
+    description:
+      'The final internship project I produced while working with Hobson at Tangible AI. I built a series of information retrieval (IR) systems to help get better search results from a corpus of health documents. Starting with TF-IDF, I built a processing and evaluation pipeline which was then used to compare results against an alternative BERT vectoriser. The internship and Hobson’s book, "NLP in Action", were greatly illuminating. I’m very grateful to the team at Tangible for being so generous with their time.',
+    technologies: ['Jupyter', 'Pandas', 'SpaCy', 'NumPy', 'SciKit', 'HuggingFace', 'Joblib', 'PyTorch'],
+    viewLink: 'https://github.com/earcherc/vector-search',
+  },
+  {
+    imageSrc: radreportImage,
+    title: 'RadReport',
+    description:
+      'A radiology reporting system that significantly improved accuracy, detail and throughput of Adelaide MRI. Using various forms of data and image analysis, RadReport represented a step forward in radiology.',
+    technologies: [
+      'Angular',
+      'RxJS',
+      'Docker',
+      'Redis',
+      'Flask',
+      'React',
+      'SQLAlchemy',
+      'Celery',
+      'PyTest',
+      'Cypress',
+      'Sentry',
+      'Jinja',
+      'Postgres',
+    ],
+    viewLink: 'https://adelaidemri.com/',
+  },
+  {
     imageSrc: michellesImage,
     title: 'Harmony Within',
     description:
-      'An elegant home for a Health Coach that allows clients to learn and transact with Michelle. Using StripeAPI for e-commerce functionality, and Acuity Scheduler for scheduling services, features core to the health coaching business model are implemented. Content management is facilitated through Sanity.io, allowing for a blog for added exposure. GatsbyJS ensures a fast and responsive experience.',
-    technologies: ['Gatsby.js', 'Stripe API', 'Sanity.io', 'Acuity Scheduler'],
+      'A health coaching business website that provides valuable information and tools for patient management. This provides the coach with an elegant and professional online presence that increases engagement and conversion of potential clientele.',
+    technologies: ['Gatsby', 'Stripe', 'Sanity.io', 'Acuity Scheduler'],
     viewLink: 'https://michellesportfoliomain.gatsbyjs.io/',
   },
-
   {
-    imageSrc: ethansImage,
-    title: 'My Portfolio',
+    imageSrc: portfolioImage,
+    title: 'Portfolio',
     description:
-      'My portfolio offers a simple and clean home for sharing thoughts and ideas through its blog. Leveraging Gatsby and TailwindCSS for rapid design and development, and React-Spring for animations, this project combines technology and design to create a lively user experience.',
-    technologies: ['Gatsby.js', 'TailwindCSS', 'React-Spring'],
-    viewLink: 'https://ethansportfolio.example.com/',
+      'I designed this portfolio to offer an elegant home for my thoughts, projects and services. It’s also used as a testing ground for code practices, technologies, and design.',
+    technologies: ['Next', 'Tailwind CSS', 'Framer motion', 'MDX', 'Vercel'],
+    viewLink: 'https://ethancavill.me',
+  },
+  {
+    imageSrc: devarityImage,
+    title: 'Devarity',
+    description:
+      'An indie game distributor for developers and gamers. The mission was to support indie developers through a custom platform that provided healthier profit margins and unique customer buying experience. At launch we featured over 50 games from our partners.',
+    technologies: ['PHP', 'MySQL', 'Python', 'Bootstrap', 'JQuery'],
   },
 ];
 
